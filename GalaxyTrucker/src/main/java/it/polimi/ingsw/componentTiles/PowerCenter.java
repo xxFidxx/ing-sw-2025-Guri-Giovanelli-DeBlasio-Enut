17140b5a1d653378a203d1d9da016d0fa395f432
xxFidxx <fidel.guri@mail.polimi.it>
2025-03-16 17:44:34 +0100

    COMMIT FID

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/componentTiles/PowerCenter.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/componentTiles/PowerCenter.java
@@ -1,4 +1,21 @@
 package it.polimi.ingsw.componentTiles;
 
-public class PowerCenter {
+import it.polimi.ingsw.Bank.BatteryToken;
+
+public class PowerCenter extends ComponentTile {
+    private BatteryToken[] batteries;
+
+
+    public PowerCenter(ConnectorType[] connectors, Direction direction, BatteryToken[] batteries) {
+        super(connectors, direction);
+        this.batteries = batteries;
+    }
+
+    public BatteryToken[] getBatteries() {
+        return batteries;
+    }
+
+    public void setBatteries(BatteryToken[] batteries){
+        this.batteries = batteries;
+    }
 }
