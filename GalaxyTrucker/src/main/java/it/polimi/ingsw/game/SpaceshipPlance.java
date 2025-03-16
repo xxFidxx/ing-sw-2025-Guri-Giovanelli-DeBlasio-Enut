64a8561d6769b700d78673717e5f56b69a63fd3c
ClaudioDebla03 <claudio.deblasio@mail.polimi.it>
2025-03-16 17:50:01 +0100

    commit di prova

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/game/SpaceshipPlance.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/game/SpaceshipPlance.java
@@ -1,6 +1,9 @@
 package it.polimi.ingsw.game;
 
+import it.polimi.ingsw.Bank.CosmicCredit;
 import it.polimi.ingsw.componentTiles.ComponentTile;
+
+import java.util.List;
 
 public class SpaceshipPlance {
     private ComponentTile[][] components;
@@ -12,5 +15,27 @@ public class SpaceshipPlance {
 
     public boolean checkCorrectness(){
 
+    }
+
+    public static class Player {
+        private String nickname;
+        private Placeholder placeholder;
+        private SpaceshipPlance spaceshipPlance;
+        private List<CosmicCredit> credits;
+
+        public String getNickname() {
+            return nickname;
+        }
+
+        public Placeholder getPlaceholder() {
+            return placeholder;
+        }
+
+        public SpaceshipPlance getSpaceshipPlance() {
+            return spaceshipPlance;
+        }
+        public List<CosmicCredit> getCredits() {
+            return credits;
+        }
     }
 }
