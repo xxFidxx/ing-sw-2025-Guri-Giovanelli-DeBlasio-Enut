805ba83f8a7fd1760862b4006bc78e63b7aedfeb
ClaudioDebla03 <claudio.deblasio@mail.polimi.it>
2025-03-18 17:20:03 +0100

    commit di prova

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/game/Flightplance.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/game/Flightplance.java
@@ -1,23 +1,39 @@
 package it.polimi.ingsw.game;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import java.util.Arrays;
 
 public class Flightplance {
     private Placeholder[] spots;
     private Deck[] decks;
+    private Game game;
 
-    public Flightplance(Placeholder[] spots, Deck[] decks) {
+    public Flightplance(Placeholder[] spots, Deck[] decks, Game game) {
         this.spots = spots;
         this.decks = decks;
+        this.game = game;
     }
 
     public Deck[] getDecks() {
         return decks;
     }
 
-    public Placeholder[] getNext(Placeholder placeholder) {}
+    public Optional<Player> getNext(Player player) {
+
+    }
 
     public void move(Placeholder placeholder, int num){}
 
-    public Placeholder[] getFirst() {}
+    public Optional <Player> getFirst() {
+        return Arrays.stream(game.getPlayer()).max(Comparator.comparingInt(p->p.getPlaceholder().getPosizione()));
+
+
+
+
+
+    }
 
     public Placeholder[] getSpots() {
         return spots;
