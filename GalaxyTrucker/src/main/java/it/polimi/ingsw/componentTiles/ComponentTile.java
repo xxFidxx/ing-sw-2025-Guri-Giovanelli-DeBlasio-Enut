800d2f720789c6a45208c583bab507e212cec0cc
MatteoEnut <matteo.enut@mail.polimi.it>
2025-03-24 20:30:56 +0100

    aggiunto logica rotazione per ComponentTile

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/componentTiles/ComponentTile.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/componentTiles/ComponentTile.java
@@ -3,14 +3,35 @@ package it.polimi.ingsw.componentTiles;
 import java.awt.*;
 
 public abstract class ComponentTile {
-    private final ConnectorType[] connectors;
-    protected Direction direction;
-    private ComponentTile[] connectedComponents;
+    private final ConnectorType[] connectors; // ruotati insieme alla carta dovrebbero avere anche tipi speciali tipo cannone etc...
+    protected Direction direction; // reso inutile
 
     public ComponentTile(ConnectorType[] connectors,Direction direction){
         this.connectors = connectors;
         this.direction = direction;
     }
+
+    public void rotateClockwise() {
+        ConnectorType last = connectors[3];
+
+        for (int i = 3; i > 0; i--) {
+            connectors[i] = connectors[i-1];
+        }
+
+        connectors[0] = last;
+    }
+
+    public void rotateCounterClockwise() {
+        ConnectorType first = connectors[0];
+
+        for (int i = 0; i < 3; i++) {
+            connectors[i] = connectors[i+1];
+        }
+
+        connectors[3] = first;
+    }
+
+
     public ConnectorType[] getConnectors() {
         return connectors;
     }
@@ -18,7 +39,5 @@ public abstract class ComponentTile {
     public Direction getDirection() {
         return direction;
     }
-
-
 
 }
