bed606bbf495b18988db713c20833ce860bfd775
xxFidxx <fidel.guri@mail.polimi.it>
2025-06-21 20:28:21 +0200

    gridImages in assembly GUI

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/gui/AssemblyController.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/gui/AssemblyController.java
@@ -5,16 +5,32 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
 public class AssemblyController extends Controller {
+    private static final Image COVERED_CARD_IMAGE;
+
+    static {
+        try (InputStream in = AssemblyController.class.getResourceAsStream("/coveredCard.jpg")) {
+            File tempFile = File.createTempFile("coveredCard", ".jpg");
+            tempFile.deleteOnExit();
+            Files.copy(in, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            COVERED_CARD_IMAGE = new Image(tempFile.toURI().toString());
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load covered card image", e);
+        }
+    }
 
     @FXML private GridPane coveredTilesGrid;
 
     public void initialize() {
-        Image tileImage = new Image(getClass().getResourceAsStream("/coveredCard.jpg"));
-
         for (int row = 0; row < 12; row++) {
             for (int col = 0; col < 13; col++) {
-                ImageView imageView = new ImageView(tileImage);
+                ImageView imageView = new ImageView(COVERED_CARD_IMAGE);
                 imageView.setFitWidth(30);
                 imageView.setFitHeight(30);
                 coveredTilesGrid.add(imageView, col, row);
