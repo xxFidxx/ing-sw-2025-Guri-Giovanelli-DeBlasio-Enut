3d975171d4b1dfb91e0e047065021eff4c2f5473
xxFidxx <fidel.guri@mail.polimi.it>
2025-06-21 18:21:56 +0200

    fixing bug in epidemic

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/model/componentTiles/Cabin.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/model/componentTiles/Cabin.java
@@ -34,6 +34,6 @@ public class Cabin extends ComponentTile implements Serializable {
 
     @Override
     public String toString() {
-        return super.toString() + "isCentral=" + isCentral + " id: " + getId() + Arrays.toString(lifeSupportSystemColors);
+        return super.toString() + "isCentral=" + isCentral + " id: " + getId() + Arrays.toString(figures);
     }
 }
