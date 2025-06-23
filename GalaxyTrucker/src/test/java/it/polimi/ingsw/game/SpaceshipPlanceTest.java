d03b430297679dcde65c1038d892c5d52d73d8c5
Andrea <andrea1.giovanelli@mail.polimi.it>
2025-06-23 16:22:25 +0200

    testing SpaceshipPlance

---
+++ GalaxyTrucker/src/test/java/it/polimi/ingsw/game/SpaceshipPlanceTest.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.game;
+import static org.junit.Assert.*;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.*;
+
+import java.util.*;
+
+public class SpaceshipPlanceTest {
+}
