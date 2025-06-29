8c43f55840c560b42ec93a55a4e8740abb212317
MatteoEnut <matteo.enut@mail.polimi.it>
2025-06-29 18:39:06 +0200

    aggiunto endScreen

--- GalaxyTrucker/src/main/java/it/polimi/ingsw/gui/pageControllers/EndController.java
+++ GalaxyTrucker/src/main/java/it/polimi/ingsw/gui/pageControllers/EndController.java
@@ -2,11 +2,18 @@ package it.polimi.ingsw.gui.pageControllers;
 
 import it.polimi.ingsw.gui.Controller;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class EndController extends Controller {
+
+    @FXML private Label endTextLabel;
 
     @FXML
     private void handleNext() {
         sceneManager.switchTo("home");
+    }
+
+    public void setText(String text) {
+        endTextLabel.setText(text);
     }
 }
